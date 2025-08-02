8d04834eeb0ff3eef9e2b4cd379540b5778b8318
Rafshan Jani <140574572+Adnan2-a11y@users.noreply.github.com>
2025-08-02 16:57:33 +0600

    Refactor user model and update FastAPI routes for room booking functionality

--- app/settings/model.py
+++ app/settings/model.py
@@ -1,9 +1,1 @@
-
-from sqlmodel import Field, SQLModel
-from typing import Optional
-
-class User(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str = Field(index=True, nullable=False)
-    email: str = Field(index=True, unique=True, nullable=False)
-
+from sqlalchemy import Column, Integer, String, Boolean
--- main.py
+++ main.py
@@ -1,42 +1,16 @@
-from fastapi import FastAPI,Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-from contextlib import asynccontextmanager
-
-from routers.users import router as user_router
-from routers.rooms import router as room_router
-from routers.houses import router as house_router
+from fastapi import FastAPI,Request, HTTPException,Depends
+from pydantic import BaseModel
+from typing import List,Annotated 
 
 
-@asynccontextmanager
-async def lifespan(app : FastAPI):
-    print("Starting up the app...")
-    yield
-    print("Shutting down the app...")#.\env\Scripts\activate.ps1 
-
-app = FastAPI(title="FastAPI Example",
-              description="A simple FastAPI application",
-              version="1.0.0",
-              lifespan=lifespan )
-
-app.include_router(user_router)
-app.include_router(room_router)
-app.include_router(house_router)
-app.lifespan_context = lifespan
-
-
-#app.get("/")
-#sync def Home():
-#   return {"message ": "Hello World!",
-#           "author": "Make Curies",
-#           "version": "1.0.0",}
-
-#@app.get("/health")
-#async def Health_Cheak():
-#    return {"Status": "OK"} 
-
-templates = Jinja2Templates(directory = "templates")
-@app.get("/",response_class= HTMLResponse)
-async def serve_home(request : Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
+class Room_Booking(BaseModel):
+    room_id: int
+    user_id: int
+    start_time: str
+    end_time: str
+class Room_Booking_response(BaseModel):
+    room_id: int
+    user_id: int
+    start_time: str
+    end_time: str
+    status: str
